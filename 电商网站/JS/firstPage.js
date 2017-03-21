246bf56aeb1b0e09344d546e97924e6e5b21877b
laoboxie <594502135@qq.com>
2017-03-21 20:30:14 +0800

    此次上传整体项目
    包括图片资源、HTML、CSS、JavaScript文件

---
+++ 电商网站/JS/firstPage.js
@@ -0,0 +1,113 @@
+window.onload=function(){
+	navigation();	//导航跳转页面
+	list_items_show();	//绑定商品分类hover
+	// alert("1");
+
+	big_banner_show();	//绑定大banner图片切换的hover
+	small_banner_show();	//绑定小banner图片切换的hover
+
+	login_box_show();	//绑定登录点击事件
+
+
+}
+function navigation(){
+	var header_nav_tab = document.getElementById('header_nav_tab');
+	var lis = header_nav_tab.getElementsByTagName("li");
+	for (var i = 0; i < lis.length; i++) {
+		lis[i].index=i;
+		lis[i].onclick=function(){
+			var location;
+			switch(this.index) {
+				case 0: location = "firstPage.html";break;
+				case 1: location = "productCategory.html";break;
+				case 2: location = "selectPage.html";break;
+				case 3: location = "goodIntroduce.html";break;
+				case 4: location = "shoppingCart.html";break;
+			}
+			window.location.href=location;
+			// for (var i = 0; i < lis.length; i++) {
+			// 	lis[i].class="";
+			// }
+			// this.class="active";
+		}
+	}
+}
+function list_items_show(){
+	var lists_item = document.getElementById('all_lists_items');
+	var items = lists_item.getElementsByClassName('all_lists_item');
+	var detail_item = document.getElementById('detail_item');
+	for (var i = 0; i < items.length; i++) {
+		items[i].onmouseover=function(){
+			detail_item.className="shopClass_list show";
+		}
+		items[i].onmouseout=function(){
+			detail_item.className="shopClass_list hide";
+		}
+	}
+}
+//banner图片切换 通用
+function banner_img_switch(btns,show_area,img_width){
+	for (var i = 0; i < btns.length; i++) {
+		btns[i].index = i;
+		btns[i].onmouseenter=function(){
+			var left = -(this.index)*img_width;
+			show_area.style.left = left+'px';
+			for (var i = 0; i < btns.length; i++) {
+				btns[i].className = "btn";
+			}
+			this.className = "btn active";
+		}
+	}
+}
+//获得大banner
+function big_banner_show(){
+	var big_banner = document.getElementById('big_banner');
+	if(big_banner==undefined){
+		return;
+	}
+	var btns = big_banner.getElementsByClassName('btn');
+	var show_area = big_banner.getElementsByClassName('banner_imgs')[0];
+	banner_img_switch(btns,show_area,811);
+}
+//获得小banner
+function small_banner_show(){
+	var good_show_area = document.getElementById('good_show');
+	if(good_show_area==undefined){
+		return;
+	}
+	var good_show_floor = good_show_area.getElementsByClassName('good_show_floor');
+	for (var i = 0; i < good_show_floor.length; i++) {
+		var banner_small = good_show_floor[i].getElementsByClassName('banner_small')[0];
+
+		var btns = banner_small.getElementsByClassName('btn');
+		var show_area = banner_small.getElementsByClassName('banner_imgs')[0];
+		banner_img_switch(btns,show_area,190);
+	}
+}
+function login_box_show(){	//显示登录盒子
+	var login_link = document.getElementById('login_link');
+	var mask = document.getElementById('mask');
+	var login_box = document.getElementById('login_box');
+	if( login_link==undefined ){
+		return;
+	}
+	login_link.onclick=function(){
+		mask.style.display="block";
+		login_box.style.display="block";
+		set_login_box_pos();
+	}
+	var login_close = document.getElementById('login_close');
+	login_close.onclick=function(){
+		mask.style.display="none";
+		login_box.style.display="none";
+	}
+}
+function set_login_box_pos(){	//设置登录盒子的位置
+	var login_box = document.getElementById('login_box');
+	var client_h = document.documentElement.clientHeight;
+	var client_w = document.documentElement.clientWidth;
+	var box_h = login_box.offsetHeight;
+	var box_w = login_box.offsetWidth;
+	login_box.style.top=client_h/2-box_h/2+"px";
+	login_box.style.left=client_w/2-box_w/2+"px";
+}
---
+++ 电商网站/JS/shoppingCart_script.js
@@ -0,0 +1,201 @@
+// function addLoadEvent(func) { 
+// 	var oldonload = window.onload; 
+// 	if (typeof window.onload != 'function') { 
+// 		window.onload = func; 
+// 	} else { 
+// 	window.onload = function() { 
+// 		oldonload(); 
+// 		func(); 
+// 	} 
+// 	} 
+// } 
+(function(){
+	if ( typeof(document.addEventListener) != "undefined" ) { 
+		document.addEventListener("load",shopping_cart,true); 
+	} else { 
+		document.attachEvent("onload",shopping_cart); 
+	} 
+})();
+// window.onload=function () {
+function shopping_cart(){
+	// alert("5");
+	var cartTable = get("cartTable");
+	var tr = document.getElementsByTagName('tbody')[0].rows;
+	var checkall = get(".check-all");
+	var checkboxs = get(".checkbox");
+	var price = get(".price");
+	var count = get(".count");
+	//为数量增减 删除商品 绑定事件处理
+	for (var i = 0; i < tr.length; i++) {
+		tr[i].onclick=function(e){
+			e = e || window.event;
+			var el = e.srcElement;
+			var cls = el.className;
+			var num_input = this.getElementsByTagName("input")[1];
+			var num = parseInt(num_input.value);
+			var reduce = this.getElementsByTagName("span")[1];
+			switch(cls) {
+				case "reduce":
+					num = Math.max(--num,1);
+					num_input.value = num;
+					if(num<=1){
+						reduce.innerHTML="";
+					}
+					getsubtotal(this);
+					break;
+				case "add":
+					num_input.value = ++num;
+					if(num>=2){
+						reduce.innerHTML="-";
+					}
+					getsubtotal(this);
+					break;
+				case "delete":
+					var conf = confirm("确定不要我了吗?=\=")
+					if(conf){
+						this.parentNode.removeChild(this);
+					}
+					break;
+			}
+			gettotal();
+			console.log('test');
+		}
+		//键盘输入数量
+		var keyput = tr[i].getElementsByTagName('input')[1];
+		keyput.onkeyup = function(){
+			var thistr = this.parentNode.parentNode;
+			var reduce = thistr.getElementsByTagName('span')[1];
+			var val = parseInt(this.value);
+			if(isNaN(val) || val<1){
+				val = 1;
+			}
+			this.value = val;
+			if(val==1){
+				 reduce.innerHTML = '';
+			}else {
+				reduce.innerHTML = '-';
+			}
+			getsubtotal(thistr);
+			gettotal();
+			
+		}
+	}
+	//为小计做处理
+	function getsubtotal(ele){
+		var tds = ele.cells;
+		var price = parseFloat(tds[2].innerHTML);
+		var amount = parseFloat(tds[3].getElementsByTagName('input')[0].value);
+		var subtotal = parseFloat(price*amount);
+		subtotal = subtotal.toFixed(2);
+		tds[4].innerHTML = subtotal;
+		return subtotal;
+	}
+	//全选
+	for(var i=0;i<checkall.length;i++){
+		checkall[i].onclick=function(){
+			var trs = document.getElementsByTagName('tbody')[0].rows;
+			for(var j=0;j<trs.length;j++){
+				checkboxs[j].getElementsByTagName('input')[0].checked=this.checked;
+			}
+			checkall[0].checked=checkall[1].checked=this.checked;
+			gettotal();
+		}
+	}
+	//合计
+	function gettotal(){
+		var trs = document.getElementsByTagName('tbody')[0].rows;
+		var goodnum=0,total=0;
+		var HTMLstr="";
+		for (var i = 0; i < trs.length; i++) {
+			var ifchecked = trs[i].getElementsByTagName('input')[0].checked;
+			if(ifchecked){
+				total += parseFloat(trs[i].cells[4].innerHTML);
+				var nums = trs[i].getElementsByTagName('input')[1].value;
+				goodnum += parseInt(nums);
+				HTMLstr += '<div><img src="' + tr[i].getElementsByTagName('img')[0].src + '"><span class="del" index="' + i + '">取消选择</span></div>';
+			}
+		}
+		total = total.toFixed(2);
+		get("priceTotal").innerHTML = total;
+		get("selectedTotal").innerHTML = goodnum;
+		get("selectedViewList").innerHTML = HTMLstr;
+	}
+	//底部删除按钮
+	get("deleteAll").onclick=function(){
+		var trs = document.getElementsByTagName('tbody')[0].rows;
+		for (var i = 0; i < trs.length; i++) {
+			var ifchecked = trs[i].getElementsByTagName('input')[0].checked;
+			if(ifchecked){
+				trs[i].parentNode.removeChild(trs[i]);
+				i--;
+			}
+		}
+		gettotal();
+	}
+	//已选商品展示
+	get("selected").onclick=function(){
+		var foot = get('foot');
+		var selectedTotal = get("selectedTotal");
+		if (foot.className == 'foot') {
+		    if (selectedTotal.innerHTML != '0') {
+		        foot.className = 'foot show';
+		    }
+		}
+		else {
+		    foot.className = 'foot';
+		}
+	}
+	//代理展示中的取消选择事件
+	get("selectedViewList").onclick=function(e){
+		var e = e || window.event;
+		var elesrc = e.srcElement;
+		if(elesrc.className == 'del'){
+			var n = elesrc.getAttribute('index');
+			var inputone = checkboxs[n].getElementsByTagName('input')[0];
+			inputone.checked=false;
+			elesrc.parentNode.removeChild(elesrc);
+			inputone.onchange();
+		}
+		gettotal();
+
+				//没有商品倍选中时隐藏
+		var selectedTotal = get("selectedTotal");
+		var foot = get('foot');
+		if (selectedTotal.innerHTML == '0') {
+		    foot.className = 'foot';
+		}
+	}
+	//checkbox的onchange事件
+	var check_one = get(".check-one");
+	for(var i=0;i<check_one.length;i++){
+		check_one[i].onchange=function(){
+			var checkednum=0;
+			if (!this.checked) {
+				checkall[0].checked=checkall[1].checked=false;
+			} else{
+				for(var j=0;j<check_one.length;j++){
+					if(check_one[j].checked){
+						checkednum++;
+					}else{
+						break;
+					}
+				}
+			}
+			if (checkednum==check_one.length) {
+				checkall[0].checked=checkall[1].checked=true;
+			}
+		}
+	}
+	checkall[0].checked=true;
+	checkall[0].onclick();
+}
+
+
+
+function get(id){
+	if(id.substr(0,1)=='.'){
+		return document.getElementsByClassName(id.substr(1));
+	}else{
+		return document.getElementById(id);
+	}
+}
